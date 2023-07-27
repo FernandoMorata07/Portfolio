d1897c5c90fbcb5cf945228134ecf1a2d1591d0a
Fernando <fernando-5-vva@hotmail.com>
2023-07-27 19:26:53 +0200

    Portfolio revisado y finalizado

--- porfolio.js
+++ porfolio.js
@@ -38,22 +38,6 @@ liWebGitItems.forEach((liWebGitItem) => {
   });
 });
 
-// JS para los enlaces de los proyectos que no tengan DEPLOY indicarlo en un alert
-
-// const WebGitItems = document.querySelectorAll(".liWebGit");
-
-// // Recorrer cada elemento con clase "liWebGit" para verificar el atributo href
-// WebGitItems.forEach((WebGitItem) => {
-//   // Obtener el enlace <a> dentro del elemento
-//   const enlace = WebGitItem.querySelector("a");
-
-//   // Verificar si el enlace tiene el atributo href vacío
-//   if (!enlace.getAttribute("href")) {
-//     // Mostrar el alert cuando el atributo href está vacío
-//     alert("Este proyecto no pudo ser desplegado (deployed)");
-//   }
-// });
-
 const skillsItems = document.querySelectorAll(".contenedor-skills .skills");
 
 function animateSkillHighlight() {
@@ -72,26 +56,39 @@ function animateSkillHighlight() {
 
 skillsItems[0].classList.add("highlighted");
 
-// Llamar a la función de animación cada 3 segundos
+// Ejecuta la animación de las skills cada 3 Segundos
 setInterval(animateSkillHighlight, 3000);
 
 function animateSkillHighlight() {
-  // Obtener el índice de la skill actualmente iluminada
   const currentIndex = Array.from(skillsItems).findIndex((skill) =>
     skill.classList.contains("highlighted")
   );
 
-  // Remover la clase "highlighted" de la skill actualmente iluminada
   if (currentIndex >= 0) {
     skillsItems[currentIndex].classList.remove("highlighted");
 
-    // Esperar un breve momento antes de agregar la clase "highlighted" a la próxima skill
     setTimeout(() => {
-      // Calcular el índice de la próxima skill a iluminar
       const nextIndex = (currentIndex + 1) % skillsItems.length;
 
-      // Agregar la clase "highlighted" a la próxima skill a iluminar
       skillsItems[nextIndex].classList.add("highlighted");
-    }, 100); // Ajusta el tiempo según tu preferencia para el efecto de disminución antes de aumentar
+    }, 100);
   }
 }
+
+const webGitItems = document.querySelectorAll(".liWebGit");
+
+function verificarEnlace(event) {
+  event.preventDefault();
+
+  const enlace = event.currentTarget.querySelector("a");
+
+  if (!enlace.getAttribute("href")) {
+    alert("Este proyecto no pudo ser desplegado (deployed)");
+  } else {
+    window.location.href = enlace.getAttribute("href");
+  }
+}
+
+webGitItems.forEach((webGitItem) => {
+  webGitItem.addEventListener("click", verificarEnlace);
+});
